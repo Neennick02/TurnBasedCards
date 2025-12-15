e7d489dd36a222d0d714ad9085d00a273986a2ec
Yannick Garbe <Yannickgarbe02@gmail.com>
2025-12-15 13:40:43 +0100

    disable player cards on ai turn

--- Assets/_Scripts/Player/DeckManager.cs
+++ Assets/_Scripts/Player/DeckManager.cs
@@ -1,8 +1,11 @@
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeckManager : MonoBehaviour
 {
+    [SerializeField] private Button[] cardButtons;
+
     private List _deck = new List();
 
 
@@ -16,6 +19,9 @@ public class DeckManager : MonoBehaviour
 
     public void EnableCards(bool active)
     {
-
+        for (int i = 0; i < cardButtons.Length; i++)
+        {
+            cardButtons[i].enabled = active;
+        }
     }
 }
