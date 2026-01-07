a844d5f7ecc42f0b6c0f81d103cfb370765c9639
Nee_nnick <Yannickgarbe02@gmail.com>
2026-01-07 11:36:46 +0100

    cards can be drawn

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> cards = new List<Sprite>();
+
+    
+}
